08f1a9f39c0b7781a65c543a04f6d44accd2b950
Kingdon Barrett <kingdon.b@nd.edu>
2019-03-31 15:25:24 -0400

    fix web process
    if the parse is successful, the result is an array
    
    silence noisy health check

--- web.rb
+++ web.rb
@@ -3,8 +3,9 @@ require 'socket'
 server = TCPServer.new 5000
 
 while session = server.accept
+  begin
   request = session.gets
-  puts request
+  # puts request
 
   session.print "HTTP/1.1 200\r\n" # 1
   session.print "Content-Type: text/html\r\n" # 2
@@ -12,4 +13,9 @@ while session = server.accept
   session.print '{"ok":true}' #4
 
   session.close
+  rescue StandardError => e
+    # the health checker closes the connection after receiving:
+    # HTTP/1.1 200\r\n
+    # do not let this crash the web server, just silently eat it
+  end
 end
